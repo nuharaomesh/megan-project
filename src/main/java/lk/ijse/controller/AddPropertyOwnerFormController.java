016e7f32ce1ea11e8b7282013b7ea73ddaf292ac
nuharaomesh <omeshnuhara4@gmail.com>
2023-11-09 23:13:01 +0530

    create tenant,Propety Owner,Property adding pages

--- src/main/java/lk/ijse/controller/AddPropertyOwnerFormController.java
+++ src/main/java/lk/ijse/controller/AddPropertyOwnerFormController.java
@@ -2,27 +2,53 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.dto.PropertyOwnerDto;
+import lk.ijse.model.PropertyOwnerModel;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class AddPropertyOwnerFormController {
 
     @FXML
-    private TextField txtEmail;
-
+    private TextField txtPrpOwnerId;
     @FXML
     private TextField txtFirstName;
-
     @FXML
     private TextField txtLastName;
-
     @FXML
-    private TextField txtPrpOwnerId;
-
+    private TextField txtEmail;
     @FXML
     private TextField txtTel;
+    @FXML
+    private AnchorPane rootNode;
+    private PropertyOwnerModel ownerModel = new PropertyOwnerModel();
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+
+        var dto = new PropertyOwnerDto(txtPrpOwnerId.getText(), txtFirstName.getText(), txtLastName.getText(), txtEmail.getText(), txtTel.getText());
+
+        boolean isSaved = false;
+        try {
+
+            isSaved = ownerModel.savePrpOwner(dto);
+            if (isSaved) {
+
+                ButtonType ok = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+
+                Optional<ButtonType> type = new Alert(Alert.AlertType.INFORMATION, "Owner saved!!", ok).showAndWait();
+
+                if (type.orElse(ok) == ok) {
+
+                }
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
 
     }
 }
